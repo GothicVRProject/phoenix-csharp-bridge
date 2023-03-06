4d73aaf75fd4aa1a9a5703db1bdd01f7528c487e
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-06 19:11:33 +0100

    mesh triangle-Indices exported.

--- main.cc
+++ main.cc
@@ -31,21 +31,31 @@ struct Vector3 {
 
 struct Mesh {
   int verticesCount;
-  Vector3* vertices;
+  Vector3* vertices; // array
+  int vertexIndicesCount;
+  uint32_t* vertexIndices; // array
 };
 
 
 DllExport Mesh* loadWorldMesh(vdf_file* vdfContainer, char* worldFileName) {
   auto entry = getVDFEntry(vdfContainer, worldFileName);
   auto world = world::parse(entry->open(), game_version::gothic_1);
+
   auto &origVertices = world.world_mesh.vertices;
+  auto &origPolygons = world.world_mesh.polygons;
 
   auto mesh = new Mesh();
+
   mesh->verticesCount = origVertices.size();
   mesh->vertices = new Vector3[mesh->verticesCount];
-
   for (int i=0; i<mesh->verticesCount; i++) {
     mesh->vertices[i] = {origVertices[i].x, origVertices[i].y, origVertices[i].z};
+  }
+
+  mesh->vertexIndicesCount = origPolygons.vertex_indices.size();
+  mesh->vertexIndices = new uint32_t[mesh->vertexIndicesCount];
+  for (int i=0; i<mesh->vertexIndicesCount; i++) {
+    mesh->vertexIndices[i] = origPolygons.vertex_indices[i];
   }
 
   return mesh;
@@ -61,8 +71,18 @@ DllExport void getMeshVertex(Mesh* mesh, int index, float* x, float* y, float* z
   *z = mesh->vertices[index].z;
 }
 
+DllExport int getWorldMeshVertexIndicesCount(Mesh* mesh) {
+  return mesh->vertexIndicesCount;
+}
+
+DllExport void getWorldMeshVertexIndex(Mesh* mesh, int index, uint32_t *value) {
+  *value = mesh->vertexIndices[index];
+}
+
+
 DllExport void disposeMesh(Mesh* mesh) {
   delete mesh->vertices;
+  delete mesh->vertexIndices;
   delete mesh;
 }
 
