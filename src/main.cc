f5aa73916c2c0404e02cdc1e8ac5a4a8de35016c
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-07 08:34:24 +0100

    debug changed

--- src/main.cc
+++ src/main.cc
@@ -8,13 +8,31 @@
 using namespace phoenix;
 
 
-int main(int argc, char** argv) {
-  auto vdf = createVDFContainer();
-  addVDFToContainer(vdf, (char*)"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Gothic\\Data\\worlds.vdf");
-  Mesh* mesh = loadWorldMesh(vdf, (char*)"world.zen");
-	std::cout << "vertices: " << getWorldVerticesCount(mesh) << std::endl;
 
-  disposeMesh(mesh);
+void printWorldData() {
+  auto vdfContainer = createVDFContainer();
+  addVDFToContainer(vdfContainer, (char*)"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Gothic\\Data\\worlds.vdf");
+
+  auto entry = getVDFEntry(vdfContainer, (char*)"world.zen");
+  auto world = world::parse(entry->open(), game_version::gothic_1);
+
+  auto &mesh = world.world_mesh;
+
+
+  std::cout << "Mesh data:" << std::endl
+    << "name=" << mesh.name
+    << "materials.size=" << mesh.materials.size() << std::endl
+    << "vertices.size=" << mesh.vertices.size() << std::endl
+    << "polygons.vertex_indices.size=" << mesh.polygons.vertex_indices.size() << std::endl
+    << "polygons.material_indices.size=" << mesh.polygons.material_indices.size() << std::endl
+    << "polygons.feature_indices.size=" << mesh.polygons.feature_indices.size() << std::endl
+    << std::endl;
+
+}
+
+
+int main(int argc, char** argv) {
+  printWorldData();
 
 	// int a;
 	// std::cin >> a;
